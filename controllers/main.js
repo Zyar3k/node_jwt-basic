fdcf2ce6534507d778f2c394d17d462451369e31
zyar3k <a.zygan@gmail.com>
2021-12-20 16:02:01 +0100

    Add basic routes & controllers

---
+++ controllers/main.js
@@ -0,0 +1,16 @@
+const login = async (req, res) => {
+  res.send("Fake login/register/signup route");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, Joe Doe`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
